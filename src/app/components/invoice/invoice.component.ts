5163d199ad8aa7db7c59a87550ac634bfeca320f
Jose Becerra <becerra.pucp@gmail.com>
2024-09-21 12:58:23 -0500

    feat: nueva funcionalidad para desgarcar cualquier tipo doc xml, zip, pdf

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -9,6 +9,8 @@ import { RowItemComponent } from '../row-item/row-item.component';
 import { TotalComponent } from '../total/total.component';
 import { FormItemComponent } from '../form-item/form-item.component';
 import { Item } from '../../models/item';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-invoice',
@@ -20,7 +22,7 @@ export class InvoiceComponent implements OnInit {
 
   invoice!: Invoice;
 
-  constructor(private service: InvoiceService) { }
+  constructor(private service: InvoiceService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.invoice = this.service.getInvoice();
@@ -32,6 +34,20 @@ export class InvoiceComponent implements OnInit {
 
   addItem(item: Item) {
     this.invoice = this.service.save(item);
+  }
+
+  downloadPdf() {
+    const path = encodeURIComponent('C:/clientes/files/20538492028/anexo/2024/6/24/20537909171/20/20538492028-20-R001-00000003.pdf');
+    const url = `http://localhost:8083/api/document/download?path=${path}`;
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9...',
+    });
+
+    this.http.get(url, { headers, responseType: 'blob' }).subscribe((response: Blob) => {
+      const filename = 'document.pdf'; // Puedes cambiarlo dinámicamente si deseas
+      saveAs(response, filename); // Guarda el archivo
+    });
   }
 
 }
